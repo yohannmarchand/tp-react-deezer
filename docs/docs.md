1fa42b0406b8b5d5b2cb8882fc2075d1b4ce752d
yohannmarchand <marchandyohannnnn@gmail.com>
2022-03-15 23:44:53 +0100

    add maquette link

---
+++ docs/docs.md
@@ -0,0 +1,2 @@
+# Maquette
+- [Lien](https://www.figma.com/file/zPyffdZlyTd8E1k3WIQ3fB/deezer-app?node-id=0%3A1)
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from "../App";
+import {Provider} from "react-redux";
+import store from "../store";
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Provider store={store}><App /></Provider>);
   const linkElement = screen.getByText(/Deezer/i);
   expect(linkElement).toBeInTheDocument();
 });
